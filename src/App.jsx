0977f6b5d03ce50a92f6107d372d45a872696d6a
Ryan Adhi <ryan.adhi@bithealth.co.id>
2021-06-18 10:00:42 +0700

    added grommet

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,40 @@
 import React, { Suspense } from 'react';
 import { Switch, Route, Link } from 'react-router-dom';
+import { Grommet, Heading } from 'grommet';
 import routes from './routes';
 function App() {
   return (
-    <div>
-      <nav>
-        <h1>This is my app</h1>
-        <ul>
-          <li>
-            <Link to='/'>Home</Link>
-          </li>
-          <li>
-            <Link to='/about'>About</Link>
-          </li>
-          <li>
-            <Link to='/date'>Date</Link>
-          </li>
-          <li>
-            <Link to='/hospital'>Hospital</Link>
-          </li>
-        </ul>
-      </nav>
-      <Suspense fallback={<></>}>
-        <Switch>
-          {routes.map(({ isPrivate = false, ...rest }, index) => (
-            <Route key={index} {...rest} />
-          ))}
-        </Switch>
-      </Suspense>
-    </div>
+    <Grommet className='App'>
+      <div>
+        <nav>
+          <h1>This is my app</h1>
+          <ul>
+            <li>
+              <Link to='/'>Home</Link>
+            </li>
+            <li>
+              <Link to='/about'>About</Link>
+            </li>
+            <li>
+              <Link to='/date'>Date</Link>
+            </li>
+            <li>
+              <Link to='/hospital'>Hospital</Link>
+            </li>
+          </ul>
+        </nav>
+        <Heading size='large' color='#00739D'>
+          I've Mastered Grommet!
+        </Heading>
+        <Suspense fallback={<></>}>
+          <Switch>
+            {routes.map(({ isPrivate = false, ...rest }, index) => (
+              <Route key={index} {...rest} />
+            ))}
+          </Switch>
+        </Suspense>
+      </div>
+    </Grommet>
   );
 }
 export default App;
