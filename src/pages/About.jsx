1e093b066314bd01a63159ad95aa399bd7f8b938
Ryan Adhi <ryan.adhi@bithealth.co.id>
2021-06-21 16:23:38 +0700

    aggrement from there

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,13 +1,50 @@
 import React, { useState } from 'react';
-import {
-  // Box,
-  // Button,
-  Layer as Dialog,
-  // Text,
-  // CheckBox
-} from 'grommet';
-// import { FormClose } from 'grommet-icons';
+import { Box, Button, Layer as Dialog, Text, CheckBox } from 'grommet';
+import { FormClose } from 'grommet-icons';
 // import agreement from '../constant/agreement';
+
+const agreement = {
+  intro: {
+    id: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit',
+    en: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit',
+  },
+  terms: [
+    {
+      desc: {
+        id: 'sed do eiusmod tempor incididunt ut labore et dolore magna aliqua',
+        en: 'sed do eiusmod tempor incididunt ut labore et dolore magna aliqua',
+      },
+    },
+    {
+      desc: {
+        id: 'Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
+        en: 'Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
+      },
+    },
+    {
+      desc: {
+        id: 'Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.',
+        en: 'Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.',
+      },
+    },
+    {
+      desc: {
+        id: 'Excepteur sint occaecat cupidatat non proident',
+        en: 'Excepteur sint occaecat cupidatat non proident',
+      },
+    },
+    {
+      desc: {
+        id: 'sunt in culpa qui officia deserunt mollit anim id est laborum.',
+        en: 'sunt in culpa qui officia deserunt mollit anim id est laborum.',
+      },
+    },
+  ],
+  check: {
+    id: 'Amet massa vitae tortor condimentum lacinia. Porta nibh venenatis cras sed felis',
+    en: 'Amet massa vitae tortor condimentum lacinia. Porta nibh venenatis cras sed felis',
+  },
+};
 
 export default function About() {
   const [isOpen, setIsOpen] = useState(true);
@@ -25,13 +62,91 @@ export default function About() {
           full='horizontal'
           margin={{ vertical: '36px' }}
         >
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
-          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-          aliquip ex ea commodo consequat. Duis aute irure dolor in
-          reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
-          pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
-          culpa qui officia deserunt mollit anim id est laborum.
+          <Box
+            overflow='auto'
+            fill
+            round='medium'
+            align='start'
+            justify='start'
+            background='white'
+            style={{ position: 'relative' }}
+          >
+            <Box
+              pad={{ horizontal: 'medium' }}
+              fill='horizontal'
+              round={{ corner: 'top', size: 'medium' }}
+              background='white'
+              flex={false}
+              style={{
+                position: 'fixed',
+                top: '0',
+                alignItems: 'flex-end',
+              }}
+            >
+              <FormClose
+                color='status-critical'
+                size='large'
+                onClick={() => setIsOpen(false)}
+                style={{ cursor: 'pointer' }}
+              />
+            </Box>
+            <Box
+              flex={false}
+              margin={{ top: '36px' }}
+              pad={{ horizontal: 'large', top: 'large' }}
+            >
+              <Text size='16px' style={{ marginBottom: '.5rem' }}>
+                {agreement.intro.id}
+              </Text>
+              <Text size='14px' style={{ fontStyle: 'italic' }}>
+                {agreement.intro.en}
+              </Text>
+              <ol>
+                {agreement.terms.map((item, index) => (
+                  <li key={index}>
+                    <Text size='16px'>{item.desc.id}</Text>
+                    <br />
+                    <Text size='14px' style={{ fontStyle: 'italic' }}>
+                      {item.desc.en}
+                    </Text>
+                  </li>
+                ))}
+              </ol>
+              <CheckBox
+                // checked={checked}
+                // onChange={onClickCheck}
+                label={
+                  <Box>
+                    <Text
+                      size='16px'
+                      style={{ lineHeight: '1.5rem', marginBottom: '0.5rem' }}
+                    >
+                      {agreement.check.id}
+                    </Text>
+                    <Text
+                      size='14px'
+                      style={{ lineHeight: '1.5rem', fontStyle: 'italic' }}
+                    >
+                      {agreement.check.en}
+                    </Text>
+                  </Box>
+                }
+              />
+            </Box>
+            <Box
+              width='100%'
+              flex={false}
+              pad={{ horizontal: 'large', bottom: 'medium' }}
+            >
+              <Button
+                primary
+                // disabled={status === 'loading' || !checked}
+                margin={{ top: '36px' }}
+                label='Kirim'
+                // onClick={onSubmit}
+              />
+            </Box>
+          </Box>
         </Dialog>
       )}
     </div>
